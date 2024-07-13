6269abef9de14341d2c4f383982c8d2abc404acf
Eugene Geym <geymzheka@gmail.com>
2024-07-13 20:17:42 +0500

    added urls for lessons

--- materials/urls.py
+++ materials/urls.py
@@ -4,7 +4,7 @@ from materials.apps import MaterialsConfig
 
 from rest_framework.routers import DefaultRouter
 
-from materials.views import CourseViewSet
+from materials.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = MaterialsConfig.name
 
@@ -12,4 +12,9 @@ router = DefaultRouter()
 router.register("", CourseViewSet)
 
 urlpatterns = [
-] + router.urls
+                  path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+                  path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
+                  path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-get'),
+                  path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+                  path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
+              ] + router.urls
