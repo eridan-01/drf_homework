efd17b298e27dd40dfcab52f6568d070f23d5d02
Eugene Geym <geymzheka@gmail.com>
2024-07-20 17:40:24 +0500

    changed url

--- materials/urls.py
+++ materials/urls.py
@@ -4,7 +4,8 @@ from materials.apps import MaterialsConfig
 
 from rest_framework.routers import DefaultRouter
 
-from materials.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, LessonDestroyAPIView
+from materials.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = MaterialsConfig.name
 
@@ -12,8 +13,8 @@ router = DefaultRouter()
 router.register("courses", CourseViewSet, basename='course')
 
 urlpatterns = [
-                  path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
                   path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
+                  path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
                   path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-get'),
                   path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
                   path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete'),
