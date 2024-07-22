4aac6b069693b15edc5e1919236ac88c951957de
Eugene Geym <geymzheka@gmail.com>
2024-07-22 21:22:06 +0500

    changed create user and serializer

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,22 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 
 from users.apps import UsersConfig
 
-from users.views import PaymentListAPIView
+from users.views import PaymentListAPIView, UserCreateAPIView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = UsersConfig.name
 
 urlpatterns = [
+    path('register/', UserCreateAPIView.as_view(), name='register'),
     path('payment/', PaymentListAPIView.as_view(), name='payment_list'),
+
+    # JWT token
+    path('login/', TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
 ]
