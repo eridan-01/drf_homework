eb23daa8c91feb5e211c0e6dd4bdca4137845402
Eugene Geym <geymzheka@gmail.com>
2024-07-25 16:14:00 +0500

    added validator youtube.com

--- materials/serializers.py
+++ materials/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
 from materials.models import Course, Lesson
+from materials.validators import YouTubeLinkValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
@@ -9,6 +10,7 @@ class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
+        validators = [YouTubeLinkValidator(field='video_url')]
 
 
 class CourseSerializer(serializers.ModelSerializer):
---
+++ materials/validators.py
@@ -0,0 +1,14 @@
+import re
+from rest_framework.serializers import ValidationError
+
+
+class YouTubeLinkValidator:
+    youtube_regex = re.compile(r'^(https?://)?(www\.)?(youtube\.com|youtu\.?be)/.+$')
+
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, attrs):
+        value = attrs.get(self.field)
+        if value and not self.youtube_regex.match(value):
+            raise ValidationError({self.field: 'Разрешены только ссылки на YouTube.'})
